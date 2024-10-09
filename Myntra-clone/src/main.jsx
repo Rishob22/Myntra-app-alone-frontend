fe224fbaf48379d4ba02ed16fddf9adef7ccbee0
Rishob22 <mondalrishob@gmail.com>
2024-10-09 19:40:38 +0530

    Removed empty comment

--- Myntra-clone/src/main.jsx
+++ Myntra-clone/src/main.jsx
@@ -27,7 +27,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={myntraStore}>
-      <RouterProvider router={router} />{" "}
+      <RouterProvider router={router} />
       {/* router object passed to the RouterProvider */}
     </Provider>
   </React.StrictMode>
